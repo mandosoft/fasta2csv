8da3745d1186c43df73df9f1c2c59eb1d4116a7a
mandosoft <thomasdtownsley@gmail.com>
2020-07-03 11:28:27 -0500

    Added lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use fasta2csv::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    println!("Using data from Fasta:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+    if let Err(e) = fasta2csv::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
 }
