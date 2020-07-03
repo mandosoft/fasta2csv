110695d2a5676c0848dcd0e75fc574d045a10bd6
mandosoft <thomasdtownsley@gmail.com>
2020-07-03 14:56:32 -0500

    Actual starting point for dev

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::process;
 
-use fasta2csv::Config;
+use fasta2csv::CSV;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = CSV::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
