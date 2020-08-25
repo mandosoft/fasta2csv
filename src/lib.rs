839873e83b8c8533816e6d9d5f66e72ae1f6bf12
mandosoft <thomasdtownsley@gmail.com>
2020-08-25 17:28:15 -0500

    Edits

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::fs::File;
+ use std::io::Write;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 }
 
 pub struct CSV {
@@ -16,9 +17,7 @@ impl CSV {
         if args.len() > 2 {
             return Err("Too many arguments provided");
         }
-
         let filename = args[1].clone();
-
         Ok(CSV {filename})
     }
 }
@@ -26,24 +25,22 @@ impl CSV {
 pub fn comma_separate(entry: &str) -> Vec<&str>{
     let split = entry.split("");
     let entry = split.collect::<Vec<&str>>();
-    println!("{:?}", entry);
     return entry;
-
 }
 
-pub fn write2csv(input_file: &String){
-    let csv = fs::File::create(input_file.to_owned() + "csv");
+pub fn write2csv(input_file: &String, entry: &String){
+    let prefix:Vec<&str> = input_file.split(".").collect();
+    let mut csv = fs::File::create(prefix[0].to_owned() + ".csv").expect("Unable to create file");;
+    csv.write_all(entry.as_bytes()).expect("Unable to write data");
 }
 
 pub fn search_delimiter<'a>(contents: String, filename: String) {
-
     let delimiter: &str = ">";
     let mut entry: String = filename.to_owned();
-
     for line in contents.lines() {
-
         if line.contains(delimiter) {
             comma_separate(&entry);
+            write2csv(&filename, &entry);
             println!("{}", entry);
             entry = line.to_owned();
         }
@@ -53,11 +50,9 @@ pub fn search_delimiter<'a>(contents: String, filename: String) {
     }
 }
 
-
 pub fn run(config: CSV) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
     let file_input = config.filename.clone();
     search_delimiter(contents, config.filename);
-    write2csv(&file_input);
     Ok(())
 }
